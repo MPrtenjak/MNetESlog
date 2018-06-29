8d81632015d65bd193dfd93c38cd9d2a6321dad7
Matjaž Prtenjak <matjaz@matjazev.net>
2018-06-29 21:54:01 +0200

    New version

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2016.135.7982")]
-[assembly: AssemblyFileVersion("1.2016.135.7982")]
+[assembly: AssemblyVersion("1.2018.180.7857")]
+[assembly: AssemblyFileVersion("1.2018.180.7857")]
